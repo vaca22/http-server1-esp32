ee14e8866f5d917d416b0d8552b32a29c43b31be
vaca <wsmslgh@gmail.com>
2022-04-10 07:47:03 -0700

    Initial commit

---
+++ main/http-server1.c
@@ -0,0 +1,163 @@
+#include <stdio.h>
+#include "nvs.h"
+#include "nvs_flash.h"
+#include <esp_wifi.h>
+#include <esp_event.h>
+#include <esp_log.h>
+#include <esp_system.h>
+#include <esp_timer.h>
+#include <sys/param.h>
+//#include "nvs_flash.h"
+#include "esp_netif.h"
+//#include "esp_eth.h"
+//#include "protocol_examples_common.h"
+
+#include <esp_http_server.h>
+
+#define MY_DNS_IP_ADDR 0x08080808 // 8.8.8.8
+
+
+static const char *TAG = "HTTPServer";
+
+esp_timer_handle_t restart_timer;
+
+static void restart_timer_callback(void *arg) {
+    ESP_LOGI(TAG, "Restarting now...");
+    esp_restart();
+}
+
+esp_timer_create_args_t restart_timer_args = {
+        .callback = &restart_timer_callback,
+        /* argument specified here will be passed to timer callback function */
+        .arg = (void *) 0,
+        .name = "restart_timer"
+};
+
+/* An HTTP GET handler */
+static esp_err_t index_get_handler(httpd_req_t *req) {
+    char *buf;
+    size_t buf_len;
+
+    /* Get header value string length and allocate memory for length + 1,
+     * extra byte for null termination */
+    buf_len = httpd_req_get_hdr_value_len(req, "Host") + 1;
+    if (buf_len > 1) {
+        buf = malloc(buf_len);
+        /* Copy null terminated value string into buffer */
+        if (httpd_req_get_hdr_value_str(req, "Host", buf, buf_len) == ESP_OK) {
+            ESP_LOGI(TAG, "Found header => Host: %s", buf);
+        }
+        free(buf);
+    }
+
+    /* Read URL query string length and allocate memory for length + 1,
+     * extra byte for null termination */
+    buf_len = httpd_req_get_url_query_len(req) + 1;
+    if (buf_len > 1) {
+        buf = malloc(buf_len);
+
+        free(buf);
+    }
+
+    /* Send response with custom headers and body set as the
+     * string passed in user context*/
+    const char *resp_str = (const char *) req->user_ctx;
+    httpd_resp_send(req, resp_str, strlen(resp_str));
+
+    return ESP_OK;
+}
+
+static httpd_uri_t indexp = {
+        .uri       = "/",
+        .method    = HTTP_GET,
+        .handler   = index_get_handler,
+};
+
+httpd_handle_t start_webserver(void) {
+    httpd_handle_t server = NULL;
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+    const char *config_page_template = "fuck%d";
+    int g=23;
+    char *config_page = malloc(strlen(config_page_template)+512);
+    sprintf(config_page, config_page_template,g);
+    indexp.user_ctx = config_page;
+
+    esp_timer_create(&restart_timer_args, &restart_timer);
+
+    // Start the httpd server
+    ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
+    if (httpd_start(&server, &config) == ESP_OK) {
+        // Set URI handlers
+        ESP_LOGI(TAG, "Registering URI handlers");
+        httpd_register_uri_handler(server, &indexp);
+        return server;
+    }
+
+    ESP_LOGI(TAG, "Error starting server!");
+    return NULL;
+}
+
+esp_netif_t *wifiAP;
+static void initialize_nvs(void)
+{
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK( nvs_flash_erase() );
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+}
+char* ap_ssid = NULL;
+void app_main(void) {
+    initialize_nvs();
+
+    ip_addr_t dnsserver;
+    esp_netif_init();
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    wifiAP = esp_netif_create_default_wifi_ap();
+    esp_netif_ip_info_t ipInfo_ap;
+    IP4_ADDR(&ipInfo_ap.ip, 192, 168, 4, 1);
+    IP4_ADDR(&ipInfo_ap.gw, 192, 168, 4, 1);
+    IP4_ADDR(&ipInfo_ap.netmask, 255, 255, 255, 0);
+    esp_netif_dhcps_stop(wifiAP); // stop before setting ip WifiAP
+    esp_netif_set_ip_info(wifiAP, &ipInfo_ap);
+    esp_netif_dhcps_start(wifiAP);
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    /* ESP WIFI CONFIG */
+
+    wifi_config_t ap_config = {
+            .ap = {
+                    .channel = 0,
+                    .authmode = WIFI_AUTH_WPA2_PSK,
+                    .ssid_hidden = 0,
+                    .max_connection = 8,
+                    .beacon_interval = 100,
+            }
+    };
+    char ap_ssid[] = "fuck";
+//
+    strlcpy((char *) ap_config.sta.ssid, ap_ssid, 5);
+    ap_config.ap.authmode = WIFI_AUTH_OPEN;
+////
+//
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &ap_config));
+
+
+    // Enable DNS (offer) for dhcp server
+    dhcps_offer_t dhcps_dns_value = OFFER_DNS;
+    dhcps_set_option_info(6, &dhcps_dns_value, sizeof(dhcps_dns_value));
+
+    // Set custom dns server address for dhcp server
+    dnsserver.u_addr.ip4.addr = htonl(MY_DNS_IP_ADDR);
+    dnsserver.type = IPADDR_TYPE_V4;
+    dhcps_dns_setserver(&dnsserver);
+
+    ESP_ERROR_CHECK(esp_wifi_start());
+    start_webserver();
+
+}
